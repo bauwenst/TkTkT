AV evaluation now uses ChainedCounters to be able to average over fixed-size buckets of samples.

src/tktkt/evaluation/context.py
@@ -9,6 +9,7 @@ import numpy as np
 from ..factories.preprocessing import IdentityPreprocessor
 from ..interfaces.tokeniser import Tokeniser, Preprocessor
 from ..util.iterables import streamProgress
+from ..util.dicts import ChainedCounter
 from .entropy import renyiEfficiency
 
 
@@ -25,18 +26,27 @@ def getIterableWithCounts(iterable: WordOrSentenceIterable) -> Iterable[Tuple[st
 
 
 @dataclass
+class AccessorDistribution:
+    accessors:  Dict[VocabRef, ChainedCounter[VocabRef]]
+    boundaries: Dict[VocabRef, int]
+
+
+@dataclass
 class AccessorDistributions:
     vocab: Dict[str,VocabRef]
-    left_of:  Dict[VocabRef, Counter[VocabRef]]
-    right_of: Dict[VocabRef, Counter[VocabRef]]
+    left_of:  AccessorDistribution
+    right_of: AccessorDistribution
 
 
 @dataclass
 class TypeAccessorSummary:  # Looks a lot like the SegmentationDiversity dataclass in TkTkT's entropy module.
     total_accessors: int=0  # Amount of non-unique accessors.
     av: int=0               # Amount of unique accessors.
+
     coverage: float=0       # Fraction which AV makes up of all possible unique accessors.
     uniqueness: float=0     # Fraction of accessors that are unique. Basically, TTR for the set of accessors of this type.
+    mcu: float=0
+
     entropic_efficiency: float=0
 
 
@@ -55,7 +65,13 @@ class AllAccessorSummaries:
     min:   DistributionAccessorSummaries  # For each type separately, picks the accessor distribution with the fewest types (i.e. the most predictable side) and copies its metrics.
 
 
-def getAccessors(tokeniser: Tokeniser, texts: WordOrSentenceIterable, split_into_disjunct_examples: Preprocessor=None) -> AccessorDistributions:
+def getAccessors(tokeniser: Tokeniser, texts: WordOrSentenceIterable, bucket_samples_every: int, split_into_disjunct_examples: Preprocessor=None) \
+        -> AccessorDistributions:
+    """
+    :param bucket_samples_every: Every type in the vocabulary has a left and right counter associated with it that counts
+                                 how many tokens of each type are left resp. right of it. Those counts are bucketed in
+                                 the order they come in with, so that later on, you can average over fixed-size windows of samples.
+    """
     if split_into_disjunct_examples is None:
         split_into_disjunct_examples = IdentityPreprocessor()
 
@@ -63,8 +79,10 @@ def getAccessors(tokeniser: Tokeniser, texts: WordOrSentenceIterable, split_into
     vocab: Dict[str,VocabRef] = dict()
 
     # Everything you have seen to the left and right of a given type.
-    left_of:  Dict[VocabRef, Counter[VocabRef]] = defaultdict(Counter)
-    right_of: Dict[VocabRef, Counter[VocabRef]] = defaultdict(Counter)
+    left_of:  Dict[VocabRef, ChainedCounter[VocabRef]] = defaultdict(lambda: ChainedCounter(bucket_samples_every))
+    right_of: Dict[VocabRef, ChainedCounter[VocabRef]] = defaultdict(lambda: ChainedCounter(bucket_samples_every))
+    left_bounds:  Dict[VocabRef, int] = defaultdict(int)
+    right_bounds: Dict[VocabRef, int] = defaultdict(int)
 
     for text,frequency in getIterableWithCounts(texts):
         for bounded_text in split_into_disjunct_examples.do(text):
@@ -85,9 +103,8 @@ def getAccessors(tokeniser: Tokeniser, texts: WordOrSentenceIterable, split_into
             # - If you are studying morphology, you probably want to have an edge around every word, because it matters
             #   much less in such cases what the exact type was that came before (there is no connection between the
             #   characters of the previous word and of the current word, only the meanings).
-            # - The ID for this start/end is -1 and doesn't appear in the vocabulary.
-            left_of[ids[0]][-1]   += frequency
-            right_of[ids[-1]][-1] += frequency
+            left_bounds[ids[0]]   += frequency
+            right_bounds[ids[-1]] += frequency
 
             if len(ids) > 1:
                 right_of[ids[0]][ids[1]]  += frequency  # Has no token to the left
@@ -99,22 +116,24 @@ def getAccessors(tokeniser: Tokeniser, texts: WordOrSentenceIterable, split_into
                 left_of[center][ids[i-1]]  += frequency
                 right_of[center][ids[i+1]] += frequency
 
-    # Make sure all IDs have a -1 entry.
+    # Make sure all IDs have an amount of bounds.
     for i in vocab.values():
-        left_of[i][-1]  += 0
-        right_of[i][-1] += 0
+        left_bounds[i]  += 0
+        right_bounds[i] += 0
 
-    return AccessorDistributions(vocab, left_of, right_of)
+    return AccessorDistributions(
+        vocab,
+        AccessorDistribution(left_of, left_bounds),
+        AccessorDistribution(right_of, right_bounds)
+    )
 
 
-def analyseAccessors(accessors: AccessorDistributions, do_count_ends: bool=True, predefined_vocab_size: Optional[int]=None) -> AllAccessorSummaries:
+def analyseAccessors(accessors: AccessorDistributions, do_count_ends_as_variety: bool=True, predefined_vocab_size: Optional[int]=None) -> AllAccessorSummaries:
     """
-    :param do_count_ends: Whether to pretend that every start/end of an example should've been counted as a unique type.
-                          The longer your examples were, the less this matters.
+    :param do_count_ends_as_variety: Whether to pretend that every start/end of an example should've been counted as a unique type
+                                     when computing AV. The longer your examples were, the less this matters.
     """
     vocab, left_of, right_of = accessors.vocab, accessors.left_of, accessors.right_of
-    # reverse_vocab = {i: t for t,i in vocab.items()}  # Not really needed when you loop over the vocab items.
-    # all_ids = set(left_of) | set(right_of)  # Should be equivalent to reverse_vocab.keys()
 
     summaries = AllAccessorSummaries(
         left=DistributionAccessorSummaries(
@@ -139,46 +158,48 @@ def analyseAccessors(accessors: AccessorDistributions, do_count_ends: bool=True,
         )
     )
 
-    def fillTypeSummary(summary: TypeAccessorSummary, accessor_counts: Counter, end_count: int,
-                        possible_accessors: int):
+    def fillTypeSummary(summary: TypeAccessorSummary, accessor_counts: ChainedCounter, end_count: int, possible_accessors: int):
         nonend_count = accessor_counts.total()
-        summary.total_accessors           = nonend_count + end_count
-        summary.av                        = len(accessor_counts) + do_count_ends * end_count
-        summary.coverage                  = len(accessor_counts) / possible_accessors
-        summary.uniqueness                = len(accessor_counts) / nonend_count
-        _, summary.entropic_efficiency, _ = renyiEfficiency(accessor_counts.values(), domain_size=possible_accessors, sample_size=accessor_counts.total(), alpha=1.0)
+        subcounter_totals = accessor_counts.subcounterSizes()
+        summary.total_accessors     = nonend_count + end_count
+        summary.av                  = accessor_counts.averageOverCountersAndIndices(lambda i,c: len(c) + do_count_ends_as_variety*int(end_count*subcounter_totals[i]/nonend_count)) if nonend_count else 0
+        summary.coverage            = accessor_counts.averageOverCounters(lambda c: len(c) / possible_accessors) if nonend_count else 0.0
+        summary.uniqueness          = accessor_counts.averageOverCounters(lambda c: len(c) / nonend_count)       if nonend_count else 1.0
+        summary.mcu                 = max(summary.coverage, summary.uniqueness)
+        summary.entropic_efficiency = accessor_counts.averageOverCounters(lambda c: renyiEfficiency(c.values(), domain_size=possible_accessors, sample_size=c.total(), alpha=1.0)[1]) if nonend_count else 0.0  # idk what to do with this default
 
     for t,i in streamProgress(vocab.items(), known_size=len(vocab), show_as="Computing type statistics"):
-        # Per-type statistics
-        #   - First, remove the special -1 entry from the type's distribution.
-        left_ends  = left_of[i].pop(-1)
-        right_ends = right_of[i].pop(-1)
+        # For each summary we have (left/right/both/minimum), generate the per-type statistics.
+        left_ends  = left_of.boundaries[i]
+        right_ends = right_of.boundaries[i]
         both_ends  = left_ends + right_ends
 
-        fillTypeSummary(summaries.left.per_type[t],  left_of[i],               left_ends,  predefined_vocab_size or len(right_of))  # About the last argument: we want to know how many possible types could appear LEFT OF type i, which is equivalent to asking how many possible types have anything RIGHT OF themselves.
-        fillTypeSummary(summaries.right.per_type[t], right_of[i],              right_ends, predefined_vocab_size or len(left_of))
-        fillTypeSummary(summaries.both.per_type[t],  left_of[i] + right_of[i], both_ends,  predefined_vocab_size or len(set(left_of) | set(right_of)))
-
+        # TODO: For types in the vocab that have 0 accessors, what should you do? Their values clearly skew the unweighted averages.
+        fillTypeSummary(summaries.left.per_type[t],  left_of.accessors[i],                         left_ends,  predefined_vocab_size or len(right_of.accessors))  # About the last argument: we want to know how many possible types could appear LEFT OF type i, which is equivalent to asking how many possible types have anything RIGHT OF themselves.
+        fillTypeSummary(summaries.right.per_type[t], right_of.accessors[i],                        right_ends, predefined_vocab_size or len(left_of.accessors))
+        fillTypeSummary(summaries.both.per_type[t],  left_of.accessors[i] + right_of.accessors[i], both_ends,  predefined_vocab_size or len(set(left_of.accessors) | set(right_of.accessors)))
         if summaries.left.per_type[t].av < summaries.right.per_type[t].av:
             summaries.min.per_type[t] = summaries.left.per_type[t]
         else:
             summaries.min.per_type[t] = summaries.right.per_type[t]
 
-    # Averages
+    # For each summary we have, compute averages across types.
     def fillAverages(distribution_summaries: DistributionAccessorSummaries):
         summaries_to_average = list(distribution_summaries.per_type.values())
         weights = [s.total_accessors for s in summaries_to_average]
 
         distribution_summaries.averages.total_accessors, distribution_summaries.weighted_averages.total_accessors = \
-            getMeanAndWeightedMean([s.total_accessors     for s in summaries_to_average],weights)
+            _getMeanAndWeightedMean([s.total_accessors     for s in summaries_to_average],weights)
         distribution_summaries.averages.av, distribution_summaries.weighted_averages.av = \
-            getMeanAndWeightedMean([s.av                  for s in summaries_to_average],weights)
+            _getMeanAndWeightedMean([s.av                  for s in summaries_to_average],weights)
         distribution_summaries.averages.coverage, distribution_summaries.weighted_averages.coverage = \
-            getMeanAndWeightedMean([s.coverage            for s in summaries_to_average], weights)
+            _getMeanAndWeightedMean([s.coverage            for s in summaries_to_average], weights)
         distribution_summaries.averages.uniqueness, distribution_summaries.weighted_averages.uniqueness = \
-            getMeanAndWeightedMean([s.uniqueness          for s in summaries_to_average], weights)
+            _getMeanAndWeightedMean([s.uniqueness          for s in summaries_to_average], weights)
+        distribution_summaries.averages.mcu, distribution_summaries.weighted_averages.mcu = \
+            _getMeanAndWeightedMean([s.mcu                 for s in summaries_to_average], weights)
         distribution_summaries.averages.entropic_efficiency, distribution_summaries.weighted_averages.entropic_efficiency = \
-            getMeanAndWeightedMean([s.entropic_efficiency for s in summaries_to_average], weights)
+            _getMeanAndWeightedMean([s.entropic_efficiency for s in summaries_to_average], weights)
 
     fillAverages(summaries.left)
     fillAverages(summaries.right)
@@ -188,7 +209,7 @@ def analyseAccessors(accessors: AccessorDistributions, do_count_ends: bool=True,
     return summaries
 
 
-def getMeanAndWeightedMean(values: list, weights: list) -> Tuple[float,float]:
+def _getMeanAndWeightedMean(values: list, weights: list) -> Tuple[float,float]:
     values  = np.array(values)
     weights = np.array(weights)
     weights = weights / np.sum(weights)

src/tktkt/util/dicts.py
@@ -1,8 +1,11 @@
-from typing import TypeVar, Dict, List, Generic, Callable
+from typing import TypeVar, Dict, List, Generic, Callable, Union, Iterator
 from collections import OrderedDict, Counter
+import numpy as np
+import numpy.random as npr
 
 K = TypeVar("K")
 V = TypeVar("V")
+Number = TypeVar("Number", bound=Union[int,float])
 
 
 def invertdict(d: Dict[K,V], noninjective_ok=True) -> Dict[V,K]:
@@ -88,16 +91,36 @@ class ChainedCounter(Counter[K], Generic[K]):
     referring to that global data structure of super().
     """
 
-    def __init__(self, max_subcounter_size: int):
+    def __init__(self, max_subcounter_size: int, seed_for_addition: int=0):
         super().__init__()  # Because this is a subclass of Counter, the super class has a "master data structure".
         self._counters = [Counter()]
         self._current_size = 0
         self._max_size     = max_subcounter_size
+        self._rng_for_addition = npr.default_rng(seed_for_addition)
 
-    def averageOverCounters(self, counter_function: Callable[[Counter],float]) -> float:
-        return 1/len(self._counters) * sum(counter_function(counter) for counter in self._counters)
+    def totalSubcounters(self) -> int:
+        return len(self._counters)
 
-    def __setitem__(self, key, value):
+    def mapCounters(self, counter_function: Callable[[Counter],V]) -> Iterator[V]:
+        return map(counter_function, self._counters)  # If the function expects two arguments, this will crash.
+
+    def subcounterSizes(self) -> List[int]:
+        return list(self.mapCounters(lambda c: c.total()))
+
+    def averageOverCounters(self, counter_function: Callable[[Counter],Number]) -> Number:
+        weights = self.subcounterSizes()
+        values  = list(self.mapCounters(counter_function))
+        return sum(w*v for w,v in zip(weights,values))/sum(weights) if weights else 0
+
+    def averageOverCountersAndIndices(self, counter_function: Callable[[int,Counter],Number]) -> Number:
+        """Use this if you need a list in your counter function indexed based on the counter."""
+        weights = self.subcounterSizes()
+        values = list(map(counter_function, range(len(self._counters)), self._counters))
+        return sum(w*v for w,v in zip(weights,values))/sum(weights) if weights else 0
+
+    ####################################################################################################################
+
+    def __setitem__(self, key: K, value):
         """
         Assume that this is never coming from an expression "object[key] = value" and always from some kind of increment
         that already includes the current value. (Note: this means that if you try object[key] = value, what will happen
@@ -121,10 +144,51 @@ class ChainedCounter(Counter[K], Generic[K]):
                 self._current_size = 0
                 self._counters.append(Counter())
 
-    def get(self, key, default=None):
+    def get(self, key: K, default=None):
         # For some unknown reason, Counter's implementation of .get() does not use .__getitem__(). As it turns out, the
         # .update() method uses .get() rather than counter[key], and hence to support .update(), it doesn't suffice to override .__getitem__() only.
         if key in self:
             return self[key]
         else:
             return default
+
+    def pop(self, key: K) -> int:
+        for counter in self._counters:
+            counter.pop(key)
+        return super().pop(key)
+
+    def __add__(self, other: "ChainedCounter[K]") -> "ChainedCounter[K]":
+        """
+        Since it's impossible to know in what order the samples of the given counter came in within its subcounters,
+        we assume they came in randomly according to the distribution they appear to have.
+        """
+        if not isinstance(other, ChainedCounter):
+            raise NotImplementedError
+
+        sum_counter = ChainedCounter(self._max_size, seed_for_addition=self._rng_for_addition.integers(0,1_000_000))
+        for counter in self._counters:
+            for k,v in counter.items():
+                sum_counter[k] += v
+
+        for counter in other._counters:
+            # Get distribution
+            keys          = list(counter.keys())
+            counts        = [counter[k] for k in keys]
+            total_count   = sum(counts)
+            probabilities = np.array([c/total_count for c in counts])
+
+            # Sample from the distribution until you run out of samples.
+            n_remaining_keys = len(keys)
+            for _ in range(total_count):
+                if n_remaining_keys == 1:
+                    sum_counter[keys[0]] += counts[0]
+                    break
+
+                key_idx = self._rng_for_addition.choice(len(keys), p=probabilities)
+                sum_counter[keys[key_idx]] += 1
+                counts[key_idx] -= 1
+                if counts[key_idx] == 0:  # This key index must never be selected again. Renormalise the remaining probabilities.
+                    probabilities[key_idx] = 0
+                    probabilities = probabilities / np.sum(probabilities)
+                    n_remaining_keys -= 1
+        return sum_counter