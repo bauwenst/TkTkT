1e4a4fd5f5a467c10697e343a6425a9c35d7128e
Thomas Bauwens <thomas.bauwens@kuleuven.be>
2025-03-22 22:42:02 +0100

    Added tokeniser multiplexer for subtokenisers with unequal domains.

--- src/tktkt/wrappers/multiplexing.py
+++ src/tktkt/wrappers/multiplexing.py
@@ -57,48 +57,16 @@ class TokeniserMultiplexer(Tokeniser):
         return "Multiplex(" + " + ".join(sub.getName() for sub in self.subtokenisers) + ")"
 
 
-class CompressiveTokeniserMultiplexer(TokeniserMultiplexer):
-
-    def select(self, pretoken: str) -> int:
-        return min(range(len(self.subtokenisers)), key=lambda i: len(self.subtokenisers[i].prepareAndTokenise(pretoken)))
-
-
-class StochasticTokeniserMultiplexer(TokeniserMultiplexer):
+class TokeniserMultiplexer_SameDomains(TokeniserMultiplexer, TokeniserWithFiniteTypeDomain):
     """
-    Sample tokenisers proportional according to a given probability mass.
-    """
-    def __init__(self, preprocessor: MultiplexedPreprocessor,
-                 subtokenisers: List[Tokeniser], probabilities: List[float]=None, seed: int=0):
-        super().__init__(preprocessor=preprocessor, subtokenisers=subtokenisers)
-
-        if probabilities is None:
-            probabilities = [1/len(subtokenisers) for _ in range(len(subtokenisers))]
-        else:
-            assert all(p >= 0 for p in probabilities)
-
-            if len(probabilities) == len(subtokenisers) - 1:
-                probabilities.append(1 - sum(probabilities))
-            else:
-                assert len(probabilities) == len(subtokenisers)
-                total = sum(probabilities)
-                probabilities = [p/total for p in probabilities]
-
-        self._rng = npr.default_rng(seed)
-        self._distribution = np.array(probabilities)
-        self._n = len(self.subtokenisers)
-
-    def select(self, pretoken: str) -> int:
-        return self._rng.choice(self._n, p=self._distribution)  # .choice() because sadly, .integers() has no probability mass argument.
-
-
-class StochasticTokeniserMultiplexer_SharedDomain(StochasticTokeniserMultiplexer, TokeniserWithFiniteTypeDomain):
-    """
-    StochasticTokeniserMultiplexer where all the multiplexed tokenisers share the same domain-to-range mapping.
+    Multiplexer where all the multiplexed tokenisers share the same domain-to-range mapping.
     Note: domain and range should be small enough to be enumerated into a set.
+
+    Calls the super constructor of TokeniserMultiplexer while keeping its select() method abstract;
+    implements all the abstract methods of TokeniserWithFiniteTypeDomain.
     """
-    def __init__(self, preprocessor: MultiplexedPreprocessor,
-                 subtokenisers: List[TokeniserWithFiniteTypeDomain], probabilities: List[float]=None, seed: int=0):
-        super().__init__(preprocessor, subtokenisers, probabilities, seed)
+    def __init__(self, preprocessor: MultiplexedPreprocessor, subtokenisers: List[TokeniserWithFiniteTypeDomain]):
+        super().__init__(preprocessor, subtokenisers)
 
         # Check whether the first tokeniser's domain<->range mapping can be used as a stand-in for all others.
         ref_tokeniser = subtokenisers[0]
@@ -131,7 +99,115 @@ class StochasticTokeniserMultiplexer_SharedDomain(StochasticTokeniserMultiplexer
         return self._domain_and_range.types()
 
 
-class StochasticTokeniserSwitch(StochasticTokeniserMultiplexer_SharedDomain):
+class TokeniserMultiplexer_DifferentDomains(TokeniserMultiplexer, TokeniserWithFiniteTypeDomain):
+    """
+    Similar to TokeniserMultiplexer_SameDomains, except now all the subtokenisers have their own domains and IDs are
+    obtained by offsetting the IDs of subtokeniser i by the size of the domain of subtokenisers 1 to i-1.
+    """
+
+    def __init__(self, preprocessor: MultiplexedPreprocessor, subtokenisers: List[TokeniserWithFiniteTypeDomain]):
+        super().__init__(preprocessor, subtokenisers)
+        self.subtokenisers: List[TokeniserWithFiniteTypeDomain] = self.subtokenisers  # (type casting)
+
+        # Verify that all IDs in the subtokenisers run from 0 to |V_i|-1.
+        vocab_sizes = []
+        for tokeniser in subtokenisers:
+            V = tokeniser.getVocabSize()
+            vocab_sizes.append(V)
+            assert set(tokeniser.ids()) == set(range(V))
+
+        # Offsets is all you need.
+        self._offsets = np.cumsum([0] + vocab_sizes[:-1]).tolist()
+
+    def tokenise(self, pretoken: str) -> List[str]:
+        idx = self.select(pretoken)
+        subtokeniser = self.subtokenisers[idx]
+        if self._use_specific_preprocessors:
+            tokens = subtokeniser.prepareAndTokenise(pretoken)
+        else:
+            tokens = subtokeniser.tokenise(pretoken)
+        return [f"{idx}_{token}" for token in tokens]  # The only way to know which token was produced by which tokeniser is to modify the tokens, since conversion from string to integer is done long after many subtokenisers have been applied to all input in the batch.
+
+    def types(self) -> Iterable[str]:
+        for idx, subtokeniser in enumerate(self.subtokenisers):
+            for typ in subtokeniser.types():
+                yield f"{idx}_{typ}"
+
+    def idToType(self, i: int) -> str:
+        idx = 0
+        while idx < len(self.subtokenisers) and i >= self._offsets[idx]:
+            idx += 1
+        idx -= 1
+
+        return f"{idx}_{self.subtokenisers[idx].idToType(i - self._offsets[idx])}"
+
+    def ids(self) -> Iterable[int]:
+        for subtokeniser, offset in zip(self.subtokenisers, self._offsets):
+            for i in subtokeniser.ids():
+                yield offset + i
+
+    def typeToId(self, t: str) -> int:
+        idx, t = t.split("_", maxsplit=1)
+        idx = int(idx)
+        return self._offsets[idx] + self.subtokenisers[idx].typeToId(t)
+
+
+class CompressiveTokeniserMultiplexer(TokeniserMultiplexer):
+    def select(self, pretoken: str) -> int:
+        return min(range(len(self.subtokenisers)), key=lambda i: len(self.subtokenisers[i].prepareAndTokenise(pretoken)))
+
+
+class StochasticTokeniserMultiplexer(TokeniserMultiplexer):
+    """
+    Sample tokenisers proportional according to a given probability mass.
+    """
+    def __init__(self, preprocessor: MultiplexedPreprocessor,
+                 subtokenisers: List[Tokeniser], probabilities: List[float]=None, seed: int=0):
+        super().__init__(preprocessor=preprocessor, subtokenisers=subtokenisers)
+
+        if probabilities is None:
+            probabilities = [1/len(subtokenisers) for _ in range(len(subtokenisers))]
+        else:
+            assert all(p >= 0 for p in probabilities)
+
+            if len(probabilities) == len(subtokenisers) - 1:
+                probabilities.append(1 - sum(probabilities))
+            else:
+                assert len(probabilities) == len(subtokenisers)
+                total = sum(probabilities)
+                probabilities = [p/total for p in probabilities]
+
+        self._rng = npr.default_rng(seed)
+        self._distribution = np.array(probabilities)
+        self._n = len(self.subtokenisers)
+
+    def select(self, pretoken: str) -> int:
+        return self._rng.choice(self._n, p=self._distribution)  # .choice() because sadly, .integers() has no probability mass argument.
+
+
+class StochasticTokeniserMultiplexer_SameDomains(StochasticTokeniserMultiplexer, TokeniserMultiplexer_SameDomains):
+    """
+    Takes its .select() implementation from StochasticTokeniserMultiplexer, and implementations for methods that have to
+    do with the token domain from TokeniserMultiplexer_SameDomains.
+    """
+    def __init__(self, preprocessor: MultiplexedPreprocessor, subtokenisers: List[TokeniserWithFiniteTypeDomain],
+                 probabilities: List[float]=None, seed: int=0):
+        super().__init__(preprocessor, subtokenisers, probabilities, seed)
+        TokeniserMultiplexer_SameDomains.__init__(self, preprocessor, subtokenisers)
+
+
+class StochasticTokeniserMultiplexer_DifferentDomains(StochasticTokeniserMultiplexer, TokeniserMultiplexer_DifferentDomains):
+    """
+    Takes its .select() implementation from StochasticTokeniserMultiplexer, and implementations for methods that have to
+    do with the token domain from TokeniserMultiplexer_DifferentDomains.
+    """
+    def __init__(self, preprocessor: MultiplexedPreprocessor, subtokenisers: List[TokeniserWithFiniteTypeDomain],
+                 probabilities: List[float]=None, seed: int=0):
+        super().__init__(preprocessor, subtokenisers, probabilities, seed)
+        TokeniserMultiplexer_DifferentDomains.__init__(self, preprocessor, subtokenisers)
+
+
+class StochasticTokeniserSwitch(StochasticTokeniserMultiplexer_SameDomains):
     """
     Multiplexer that only takes two tokenisers (with the same domain) and uses a more efficient sampler than
     npr.choice() to choose the tokeniser.
---
+++ tst/inference/test_multiplexing.py
@@ -0,0 +1,59 @@
+
+def test_sharedVocabulary():
+    """
+    Testing if the "diamond inheritance" done in the multiplexer hierarchy actually just works.
+    """
+    from transformers import AutoTokenizer
+
+    from tktkt.factories.preprocessing import IdentityPreprocessor
+    from tktkt.wrappers.multiplexing import StochasticTokeniserMultiplexer_SameDomains, MultiplexedPreprocessor
+    from tktkt.models.huggingface.wrapper import HuggingFaceTokeniser
+    from tktkt.models.greedy.directional import L2R_Greedy
+
+    core = AutoTokenizer.from_pretrained("roberta-base")
+
+    tk_samedomain = StochasticTokeniserMultiplexer_SameDomains(preprocessor=MultiplexedPreprocessor(IdentityPreprocessor(), specific_preprocessors=False), subtokenisers=[
+        HuggingFaceTokeniser(core),
+        L2R_Greedy(preprocessor=IdentityPreprocessor(), vocab=core.get_vocab())
+    ])
+
+    for _ in range(20):
+        tokens = tk_samedomain.prepareAndTokenise("expeditious")
+        ids = [tk_samedomain.typeToId(t) for t in tokens]
+        print(tokens)
+        print(ids)
+        print([tk_samedomain.idToType(i) for i in ids])
+        print()
+
+
+def test_differentVocabulary():
+    """
+    Toy example showing that two vocabularies, even when they have some overlapping types and many overlapping IDs,
+    can be multiplexed by offsetting the IDs of the second, losslessly.
+    """
+    from tktkt.wrappers.multiplexing import StochasticTokeniserMultiplexer_DifferentDomains, MultiplexedPreprocessor
+    from tktkt.models.identity.segmentation import IdentityTokeniserWithVocab
+    from tktkt.factories.preprocessing import TraditionalPreprocessor, IdentityPreprocessor
+
+    tk1 = IdentityTokeniserWithVocab(IdentityPreprocessor(), {"a": 0, "b": 1, "c": 2, "d": 3, "[UNK]": 4}, unk_type="[UNK]")
+    tk2 = IdentityTokeniserWithVocab(IdentityPreprocessor(), {"a": 0, "e": 1, "f": 2, "[UNK]": 3}, unk_type="[UNK]")
+    tk = StochasticTokeniserMultiplexer_DifferentDomains(
+        MultiplexedPreprocessor(
+            TraditionalPreprocessor(),
+            True
+        ),
+        [tk1, tk2]
+    )
+
+    for _ in range(10):
+        tokens = tk.prepareAndTokenise("a b e &")
+        ids = [tk.typeToId(t) for t in tokens]
+        print(tokens)
+        print(ids)
+        print([tk.idToType(i) for i in ids])
+        print()
+
+
+if __name__ == "__main__":
+    # test_sharedVocabulary()
+    test_differentVocabulary()
